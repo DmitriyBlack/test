8a87a876906ed1c4d03727eb5c36f49321e0cf75
Dmitriy Black <dmitriy.black@gmail.com>
2018-07-23 15:53:56 +0300

    added green and red colours

--- src/main/java/com/black/dmitriy/Main.java
+++ src/main/java/com/black/dmitriy/Main.java
@@ -1,7 +1,14 @@
 package com.black.dmitriy;
 
+import com.black.dmitriy.colour.Black;
+import com.black.dmitriy.colour.Green;
+import com.black.dmitriy.colour.Red;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("World!!");
+        new Black();
+        new Green();
+        new Red();
     }
 }
